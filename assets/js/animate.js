83b1482632ae929afddb45ea386218022a4beb1a
ooaventur <ooaventur@gmail.com>
2025-09-08 20:02:30 +0200

    Delete assets directory

--- assets/js/animate.js
+++
@@ -1,9 +0,0 @@
-
-(function(){
-  const els=document.querySelectorAll('.reveal-up');
-  if(!('IntersectionObserver' in window)){ els.forEach(el=>el.classList.add('in')); return; }
-  const io=new IntersectionObserver((entries)=>{
-    entries.forEach(e=>{ if(e.isIntersecting){ e.target.classList.add('in'); io.unobserve(e.target);} });
-  }, {threshold:.2});
-  els.forEach(el=>io.observe(el));
-})();
