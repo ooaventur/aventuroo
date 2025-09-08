49406baf9249499e86a6834f9cf38fc070c05a37
ooaventur <ooaventur@gmail.com>
2025-09-08 20:02:42 +0200

    Delete autopost directory

--- autopost/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Autopost package containing utilities for feed handling."""
-
-from .utils import parse_feed, slugify
-
-__all__ = ["parse_feed", "slugify"]
--- autopost/__main__.py
+++
@@ -1,4 +0,0 @@
-from .run_all import main
-
-if __name__ == "__main__":
-    main()
