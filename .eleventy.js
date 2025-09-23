8ab93c51b036be1fc32de660d81f19757d34c26f
ooaventur <ooaventur@gmail.com>
2025-09-23 16:46:23 +0200

    Update Eleventy input directory

--- .eleventy.js
+++ .eleventy.js
@@ -149,7 +149,7 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: "src/site",
+      input: "src",
       includes: "_includes",
       data: "_data",
       output: "_site"
