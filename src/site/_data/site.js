ff2e7759d4396095dce5f19532eec447164c4882
ooaventur <ooaventur@gmail.com>
2025-09-23 16:52:31 +0200

    Add files via upload

---
+++ src/site/_data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "AventurOO",
+  description: "AventurOO curates top stories across news, technology & AI, travel inspiration, culture, lifestyle tips, and shopping deals in one streamlined hub.",
+  url: "https://aventuroo.com",
+  author: "AventurOO",
+  adFallbackSlug: "luxury-apartment-living-off-road-and-off-grid-capability-noovo-next-campervan-2025-09-20"
+};
